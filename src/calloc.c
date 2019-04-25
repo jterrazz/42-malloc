19d7e01045c1a6d30400ccc7ce3b92b2ec5003e9
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-04-25 18:48:28 +0200

    calloc fix

--- src/calloc.c
+++ src/calloc.c
@@ -6,24 +6,21 @@
 /*   By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/21 14:42:17 by jterrazz          #+#    #+#             */
-/*   Updated: 2019/04/25 18:22:17 by jterrazz         ###   ########.fr       */
+/*   Updated: 2019/04/25 18:48:08 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 
 void *calloc(size_t count, size_t size) {
-	size_t malloc_size = size * count;
+	size_t malloc_size = size == 0 ? count * 32 : size * count;
 	void *ptr = NULL;
 	// ft_putstr("Calloc here\n");
 
 	// Check if result is bigger than size_t_max ?
-	if (size == 0) {
-		ptr = malloc(count * 32);
-	} else {
-		ptr = malloc(malloc_size);
-	}
+	ptr = malloc(malloc_size);
 	// Fill with 0
-	ft_bzero(ptr, malloc_size);
+	if (ptr)
+		ft_bzero(ptr, malloc_size);
 	return ptr;
 }
