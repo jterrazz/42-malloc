fc60a8cad47c3e7e3752e4c88f03c82bf04e05f1
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-07-21 14:43:46 +0200

    Tests

---
+++ test/test_pages/test5.c
@@ -0,0 +1,12 @@
+#include "../../inc/malloc.h"
+
+int main()
+{
+	malloc(1024);
+	malloc(1024 * 32);
+	malloc(1024 * 1024);
+	malloc(1024 * 1024 * 16);
+	malloc(1024 * 1024 * 128);
+	show_alloc_mem();
+	return (0);
+}
