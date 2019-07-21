217c231a9de58c6795586e81e69075c2ce075de2
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-07-21 14:56:55 +0200

    Test done

--- test/test_pages/test3.c
+++ test/test_pages/test3.c
@@ -1,17 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test3.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/21 14:52:33 by jterrazz          #+#    #+#             */
+/*   Updated: 2019/07/21 14:56:05 by jterrazz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/malloc.h"
+#include <string.h>
+
+#define M 1024 * 1024
+
+void print(char *s)
+{
+	write(1, s, strlen(s));
+}
 
 int main()
 {
-int i;
-char *addr;
+	char *addr1;
+	char *addr2;
+	char *addr3;
 
-i = 0;
-while (i < 1024)
-{
-addr = (char*)malloc(1024);
-addr[0] = 42;
-free(addr);
-i++;
-}
-return (0);
+	addr1 = malloc(16 * M);
+	strcpy(addr1, "Bonjours\n");
+	print(addr1);
+	addr2 = malloc(16 * M);
+	addr3 = realloc(addr1, 128 * M);
+	addr3[127 * M] = 42;
+	print(addr3);
+	return (0);
 }
